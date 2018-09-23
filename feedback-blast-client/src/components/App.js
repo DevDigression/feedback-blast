8a2fd3a20d9986d937d6492f3828f3f1fb129e22
Chris Rodgers <devdigression@gmail.com>
2018-09-22 22:51:50 -0400

    main routes

--- feedback-blast-client/src/components/App.js
+++ feedback-blast-client/src/components/App.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 import Header from "./Header";
+import Landing from "./Landing";
 
-const Landing = () => <h2>Landing</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
 const NewSurvey = () => <h2>NewSurvey</h2>;
 
-const App = () => {
-	return (
-		<div>
-			<BrowserRouter>
-				<div>
-					<Header />
-					<Route exact path="/" component={Landing} />
-					<Route exact path="/surveys" component={Dashboard} />
-					<Route exact path="/surveys/new" component={NewSurvey} />
-				</div>
-			</BrowserRouter>
-		</div>
-	);
-};
+class App extends Component {
+	componentDidMount() {
+		this.props.fetchUser();
+	}
 
-export default App;
+	render() {
+		return (
+			<div className="container">
+				<BrowserRouter>
+					<div>
+						<Header />
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/surveys" component={Dashboard} />
+						<Route path="/surveys/new" component={NewSurvey} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
+}
+
+export default connect(null, actions)(App);
---
+++ feedback-blast-client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+	return (
+		<div style={{ textAlign: "center" }}>
+			<h1>Feedback Blast</h1>
+			<h2>Blast Surveys, Boost Results</h2>
+		</div>
+	);
+};
+
+export default Landing;
