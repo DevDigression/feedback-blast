cf8f63cc79d87385ecda9b36984d542f94dbcd32
Chris Rodgers <devdigression@gmail.com>
2018-10-09 20:04:59 -0400

    centered add survey button on mobile

--- feedback-blast-client/src/components/Dashboard.js
+++ feedback-blast-client/src/components/Dashboard.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SurveyList from "./surveys/SurveyList";
+import "../css/Dashboard.css";
 
 const Dashboard = () => {
 	return (
 		<div>
 			<SurveyList />
 			<div className="fixed-action-btn">
-				<Link to="/surveys/new" className="btn-floating btn-large red">
-					<i className="material-icons pink darken-1">add</i>
+				<Link
+					to="/surveys/new"
+					className="add-survey-btn btn-floating btn-large red"
+				>
+					<i className="add-survey-icon material-icons pink darken-1">
+						add
+					</i>
 				</Link>
 			</div>
 		</div>
