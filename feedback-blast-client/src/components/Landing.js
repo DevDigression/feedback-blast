1a50cada30efb2086798a21df4d761b50a623902
Chris Rodgers <devdigression@gmail.com>
2018-10-10 20:48:42 -0400

    landing page layout

--- feedback-blast-client/src/components/Landing.js
+++ feedback-blast-client/src/components/Landing.js
@@ -9,25 +9,38 @@ const Landing = props => {
 	}
 	return (
 		<div className="landing-page" style={{ textAlign: "center" }}>
-			<h1>Feedback Blast</h1>
-			<h2>Blast Surveys, Boost Results</h2>
+			<header>
+				<h1>Feedback Blast</h1>
+				<h2>Blast Surveys, Boost Results</h2>
+			</header>
 			<div className="landing-section">
-				<div className="landing-image">
-					<img className="left" src={require("../img/feedback-comments.jpg")} />
+				<div className="landing-image left">
+					<img src={require("../img/feedback-comments.jpg")} />
 				</div>
 				<div className="landing-text">
-					<p>You want to know what your clients think, but don't have the time to ask and answer questions individually?
-					The best way to collect data is through automated survey dispatch.</p>
-					<p>Feedback Blast has you covered, with the ability to send surveys to the clients you want to hear from, to get the results that matter most!</p>
+					<p>
+						You want to know what your clients think, but don't have
+						the time to ask and answer questions individually? The
+						best way to collect data is through automated survey
+						dispatch.
+					</p>
+					<p>
+						Feedback Blast has you covered, with the ability to send
+						surveys to the clients you want to hear from, to get the
+						results that matter most!
+					</p>
 				</div>
 			</div>
 			<div className="landing-section">
-				<div className="landing-image">
-					<img className="right" src={require("../img/survey-data.jpg")} />
+				<div className="landing-image right">
+					<img src={require("../img/survey-data.jpg")} />
 				</div>
 				<div className="landing-text">
 					<ul>
-						<li>Send surveys to a list of up to 100 recipients at a time</li>
+						<li>
+							Send surveys to a list of up to 100 recipients at a
+							time
+						</li>
 						<li>Seamlessly collect feedback data</li>
 						<li>Stay focused on priority areas</li>
 					</ul>
